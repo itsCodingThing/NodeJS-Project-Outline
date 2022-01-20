0601d6ef060c619a592b5916d8b6d88236a996bf
Bhanu Pratap Singh <30897910+itsCodingThing@users.noreply.github.com>
2022-01-20 11:40:31 +0530

    Delete database directory

--- database/mongo.connection.js
+++
@@ -1,4 +0,0 @@
-mongoose = require('mongoose');
-const config = require('../config/config.json');
-dbMongoSaveConn = mongoose.createConnection(config.mongodb.connectionString, { useNewUrlParser: true });
-module.exports = dbMongoSaveConn;
